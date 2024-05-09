44475cc634ba32e67ec291db25f5859cda429476
pksilen <petri.silen@silensoft.com>
2024-05-09 12:16:05 +0300

    Update

--- src/app/components/todos/factories/createTodosElement.tsx
+++ src/app/components/todos/factories/createTodosElement.tsx
@@ -1,16 +1,13 @@
-import { List, Table, TableBody } from '@mui/material';
 import React from 'react';
 import { ViewType } from 'app/stores/controls/controlsStore';
+import { List } from '../../../common/components/list/List';
+import { Table } from '../../../common/components/table/Table';
 
 export const createTodosElement = (viewType: ViewType, todoElements: React.ReactNode) => {
   if (viewType === 'list') {
     return <List>{todoElements}</List>;
   } else if (viewType === 'table') {
-    return (
-      <Table>
-        <TableBody>{todoElements}</TableBody>
-      </Table>
-    );
+    return <Table>{todoElements}</Table>;
   }
 
   throw new Error('Unsupported view type');
