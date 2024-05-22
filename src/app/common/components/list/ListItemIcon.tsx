db6b188d9d2f4b030ffc7f90a14d22011152d735
Petri Silen <petri.silen@nokia.com>
2024-05-22 11:26:13 +0300

    Update

--- src/app/common/components/list/ListItemIcon.tsx
+++ src/app/common/components/list/ListItemIcon.tsx
@@ -1,8 +1,8 @@
 import { ListItemIcon as MuiListItemIcon } from '@mui/material';
 import React from 'react';
 
-type Props = {
-  readonly icon: React.ReactNode;
-};
+type Props = Readonly<{
+  icon: React.ReactNode;
+}>;
 
 export const ListItemIcon = ({ icon }: Props) => <MuiListItemIcon>{icon}</MuiListItemIcon>;
